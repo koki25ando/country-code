b102dbee89308173e1168fda72b55b13ceb072a6
Koki Ando <basket.koki.bowl@gmail.com>
2018-03-14 21:10:04 +1100

    country code dataset

---
+++ country_code.R
@@ -0,0 +1,12 @@
+# Packages required
+library(rvest)
+library(stringr)
+library(tidyverse)
+
+# Dataset comes from this web page (http://www.nationsonline.org/oneworld/country_code_list.htm)
+ccountry.page <- read_html("http://www.nationsonline.org/oneworld/country_code_list.htm")
+tab <- ccountry.page %>% html_table(header = TRUE)
+country.code <- tab %>% extract2(3)
+country.code <- country.code[,2:4]
+names(country.code) <- c("Country name", "code_2digit","code_3digit")
+country.code <- country.code[-1,]
