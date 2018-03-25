e5d656417c936b6045e567c37ac8d1d57346244c
Koki Ando <basket.koki.bowl@gmail.com>
2018-03-26 10:52:22 +1100

    fixes

--- country_code.R
+++ country_code.R
@@ -2,11 +2,14 @@
 library(rvest)
 library(stringr)
 library(tidyverse)
+library(magrittr)
 
 # Dataset comes from this web page (http://www.nationsonline.org/oneworld/country_code_list.htm)
 ccountry.page <- read_html("http://www.nationsonline.org/oneworld/country_code_list.htm")
 tab <- ccountry.page %>% html_table(header = TRUE)
 country.code <- tab %>% extract2(3)
 country.code <- country.code[,2:4]
-names(country.code) <- c("Country name", "code_2digit","code_3digit")
+names(country.code) <- c("Country_name", "code_2digit","code_3digit")
 country.code <- country.code[-1,]
+
+write.csv(country.code, "country_code.csv")
